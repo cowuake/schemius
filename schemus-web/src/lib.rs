687bc85609f398e8908e39edd33744ff0aa65d7a
r-mura <riccardo_mura@yahoo.com>
2023-10-24 17:01:48 +0200

    Preparing for schemus-web

---
+++ schemus-web/src/lib.rs
@@ -0,0 +1,12 @@
+use schemus::core::interpreter::Interpreter;
+use wasm_bindgen::prelude::*;
+
+// #[wasm_bindgen]
+// extern {
+//     pub fn alert(s: &str);
+// }
+
+#[wasm_bindgen]
+pub fn evaluate(expression: &str) -> String {
+    Interpreter::new().eval_expression_and_format(expression.to_string())
+}
