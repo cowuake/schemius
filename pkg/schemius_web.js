42015ff6f874e4417f2977031b39f9cc89cf5ab6
cowuake <cowuake@users.noreply.github.com>
2023-11-17 07:31:35 +0000

    deploy: e1eb461f9d122391c2e654e1ec997c60ef4ac5bd

--- pkg/schemius_web.js
+++ pkg/schemius_web.js
@@ -176,7 +176,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('schemus_web_bg.wasm', import.meta.url);
+        input = new URL('schemius_web_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
