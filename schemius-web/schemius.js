b07c1024c95763b5cd8369755b1abcfb16ac0b41
cowuake <riccardo_mura@yahoo.com>
2024-05-01 18:44:01 +0200

    Improve welcome message

--- schemius-web/schemius.js
+++ schemius-web/schemius.js
@@ -20,11 +20,11 @@ class Schemius {
       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚═╝ ╚═════╝ ╚══════╝
 
         Welcome to Schemius!      [DATETIME_PLACEHOLDER]
-         ---------------------------------------------------------
-          Press [Ctrl + K]        -> Show essential keymap
-          (environment-bindings)  -> Show bindings in current env
-          (fact 2000)             -> Big number coming!
-         ---------------------------------------------------------
+         --------------------------------------------------------
+          Press [Ctrl + K]       -> Show essential keymap
+          (environment-bindings) -> Show bindings in current env
+          (fact 2000)            -> Big number coming!
+         --------------------------------------------------------
         Go through the code at https://github.com/cowuake/schemius
     `;
   }
