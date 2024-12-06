aafd44379651b1f84d0921680ca035e9c5d6465c
cowuake <cowuake@users.noreply.github.com>
2024-12-06 06:20:31 +0000

    deploy: 1a70da55d07cdf44310947aa468c1f0d9874219a

--- schemius.js
+++ schemius.js
@@ -19,7 +19,7 @@ class Schemius {
       ███████║╚██████╗██║  ██║███████╗██║ ╚═╝ ██║██║╚██████╔╝███████║
       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚═╝ ╚═════╝ ╚══════╝
 
-        Welcome to Schemius!   [Published 2024-12-04 06:18:05 UTC]
+        Welcome to Schemius!   [Published 2024-12-06 06:20:18 UTC]
          --------------------------------------------------------
           Press [Ctrl + K]       -> Show essential keymap
           (environment-bindings) -> Show bindings in current env
